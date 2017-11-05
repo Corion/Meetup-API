2afc66c5a995056972eb399bb3c9f6508cb9578c
Max Maischein <corion@corion.net>
2017-11-05 18:51:57 +0100

    Add prerequisites and first round of real synchronisation
    This relies on a hack of Net::CalDAVTalk, at least until I understand
    how ->UpdateEvent() is supposed to work...

--- examples/events-to-ical.pl
+++ examples/events-to-ical.pl
@@ -1,9 +1,13 @@
 #!perl -w
 use strict;
+use Filter::signatures;
+use feature 'signatures';
+no warnings 'experimental::signatures';
 use POSIX qw(strftime);
 use Meetup::API;
+use Meetup::ToICal qw(meetup_to_icalendar get_meetup_event_uid);
+
 use Data::Dumper;
-use Net::CalDAVTalk;
 
 use vars '$VERSION';
 $VERSION = '0.01';
@@ -32,10 +36,14 @@ Existing events will not be overwritten.
   --calendar    Name of the CalDAV calendar
   --server      Server of the CalDAV calendar (also, credentials)
   --group       URL name of the Meetup group
+  --exclude     Don't include events matching this re
+  --verbose     show more information
+  --dry-run     don't update the calendar
 
 =head1 EXAMPLE
 
-  events-to-ical.pl --group 'Perl-User-Groups-Rhein-Main' [VCard filename or CalDAV phonebook URLs]
+  events-to-ical.pl --group Perl-User-Groups-Rhein-Main --server https://calendar.example.com/ --calendar MyEvents
+  events-to-ical.pl --group Perl-User-Groups-Rhein-Main --server calendar.example.com
 
 =cut
 
@@ -43,11 +51,18 @@ use Getopt::Long;
 use Pod::Usage;
 
 GetOptions(
-    'calendar:s' => \my $davcalendar,
-    'server:s' => \my $davserver,
+    'c|calendar:s' => \my $davcalendar,
+    's|server:s' => \my $davserver,
     #'u|user:s' => \my $username,
     #'p|pass:s' => \my $password,
     'g|group:s' => \my $groupname,
+    'sync-file:s' => \my $sync_file,
+    
+    'x|exclude:s' => \my @exclude,
+    
+    'f|force'     => \my $force,
+    'n|dry-run'   => \my $dryrun,
+    'v|verbose'   => \my $verbose,
     'help!' => \my $opt_help,
     'version!' => \my $opt_version,
 ) or pod2usage(-verbose => 1) && exit;
@@ -61,6 +76,14 @@ if( $opt_version ) {
 $groupname ||= 'Perl-User-Groups-Rhein-Main';
 $davcalendar ||= 'Default';
 my $today = strftime '%Y-%m-%dT00:00:01', localtime;
+my $syncToken;
+if( $sync_file and -r $sync_file ) {
+    open my $fh, '<', $sync_file
+        or die "Couldn't open '$sync_file': $!";
+    binmode $fh;
+    $syncToken = <$fh>;
+    $syncToken =~ s!\s+$!!;
+};
 my $meetup = Meetup::API->new();
 
 # Frankfurt am Main
@@ -68,71 +91,78 @@ my $meetup = Meetup::API->new();
 
 $meetup->read_credentials;
 
-#print Dumper $meetup->group('Perl-User-Groups-Rhein-Main')->get;
 my $events = $meetup->group_events($groupname)->get;
-        #use Data::Dumper;
-        #warn Dumper $events->[1];
-        #exit;
 
-sub entry_is_different {
-    my( $entry, $match ) = @_;
+$Data::Dumper::Sortkeys = 1;
 
-    my %numbers = map {
-        $_->content => 1,
-    } @{ $entry->numbers };
-
-    #my %match_numbers = map {
-    #    $_->content => 1,
-    #} @{ $match->numbers };
-
-    # check name or number
-    # If one of the two is a mismatch, we are different
-    #$match->name ne $entry->name
-    #    or grep { $numbers{ $_->content } } @{ $c->numbers }
-    #} @$entries;
-};
-
-sub get_meetup_event_uid {
-    my( $event ) = @_;
-    my $uid = $event->{id} . '@meetup.com';
+sub verbose(@msg) {
+    if( $verbose ) {
+    no warning 'wide';
+        print "$_\n" for @msg
+    };
 }
 
 sub add_event {
     my( $caldav, $calendar, $event ) = @_;
-    my $handle = $caldav->NewEvent( $calendar, meetup_to_icalendar( $event ));
-    #print Dumper $handle;
+    if( ! $dryrun ) {
+        my $data = meetup_to_icalendar( $event );
+        my $handle = $caldav->NewEvent( $calendar, $data);
+    } else {
+        print "Would add event\n";
+    };
 }
 
-sub meetup_to_icalendar {
-    my( $meetup ) = @_;
-    my $uid = get_meetup_event_uid( $meetup );
-    if( $meetup->{time} !~ /^(\d+)\d\d\d$/ ) {
-        warn "Weirdo timestamp '$meetup->{time}' for event";
-        return;
+sub update_event {
+    my( $caldav, $href, $event ) = @_;
+    if( ! $dryrun ) {
+        my $data = meetup_to_icalendar( $event );
+        #warn Dumper $data;
+        my $handle = $caldav->UpdateEvent( $href, $data);
+    } else {
+        print "Would add event\n";
     };
-    my $start_epoch = $1;
-    
-    # We chuck everything into the floating "local" timezone
-    my $startTime = strftime( '%Y-%m-%dT%H:%M:%SZ', gmtime( $start_epoch + ($meetup->{utc_offset} / 1000)));
-    #my $tzName = sprintf 'Etc/UTC+%02d', ($meetup->{utc_offset} / 1000 / 3600);
-    return {
-        uid      => $uid,
-        title    => $meetup->{name},
-        start    => $startTime,
-        #timeZone => $tzName,
-        #duration => 3600,
-        links    => {
-            href => { value => $meetup->{link} },
-            title => { value => 'Meetup Link' },
-        },
-        locations => {
-            name => { name => $meetup->{venue}->{name} },
-            address => {
-                value   => join( "\n", $meetup->{venue}->{address_1}, $meetup->{venue}->{city})
-            }
-        },
+}
+
+sub as_ical( $caldavArgs ) {
+    Net::CalDAVTalk->_argsToVCalendar($caldavArgs)->{entries}->[0]
+}
+
+sub ical_prop( $ical, $property ) {
+    if( my $p = $ical->property($property)) {
+        $p->[0]->value
+    } else {
+        undef
     }
 }
+
+sub entry_is_different( $dav, $meetup ) {
+    my $meetup_ical = as_ical( meetup_to_icalendar( $meetup ));
+
+    #warn Dumper $dav;
+    my $dav_ical = as_ical( $dav );
+    
+    my %differences;
+    
+    for my $attribute (qw( dtstart location )) {
+        my $upstream = ical_prop( $meetup_ical, $attribute );
+        my $local = ical_prop( $dav_ical, $attribute );
+        
+        # Exclude whitespace changes
+        for ($local,$upstream) {
+            s!\s+! !g if defined $_;
+        };
+        
+        if( $local ne $upstream ) {
+            $Data::Dumper::Useqq = 1;
+            #warn "Meetup " . Dumper $upstream;
+            #warn "DAV    " . Dumper $local;
+            verbose( "$attribute has changed from '$local' to '$upstream'");
+            $differences{ $attribute } = $upstream;
+        }
+    };
+
+    scalar keys %differences
+};
 
 if( -f $davcalendar ) {
     require Net::CalDAVTalk;
@@ -156,14 +186,25 @@ if( -f $davcalendar ) {
     );
 
     my $calendar = $CalDAV->GetCalendar($davcalendar);
-    my $upstream_events = $CalDAV->GetEvents($davcalendar, after => $today );
+    my ( $upstream_events, $removed, $errors );
+
+    if( $syncToken and !$force) {
+        ( undef, $removed, $errors ) = $CalDAV->SyncEvents($davcalendar, after => $today, syncToken => $syncToken );
+    };
+    $upstream_events = $CalDAV->GetEvents($davcalendar, after => $today );
+    
+    # The user deleted these on their DAV calendar, so we won't re-sync
+    # these unless --force'd
+    my %dav_deleted = map {
+        $_ => 1
+    } @$removed;
+    %dav_deleted = () if $force;
+
     my %upstream_events = map {
         $_->{uid} => $_
     } @$upstream_events;
-    for my $event (@$events) {
-        #use Data::Dumper;
-        #warn Dumper $event;
-        #exit;
+
+    EVENT: for my $event (@$events) {
         # Convert new event, for easy comparison
         my $uid = get_meetup_event_uid( $event );
         
@@ -174,15 +215,40 @@ if( -f $davcalendar ) {
         my $start_epoch = $1;
         my $name = sprintf "%s at %s", $event->{name}, strftime( '%Y%m%dT%H%M%SZ', gmtime( $start_epoch ));
 
-        if( exists $upstream_events{ $uid }) {
+        for my $exclude (@exclude) {
+            if( $event->{name} =~ /$exclude/ ) {
+                verbose("'$name' excluded (/$exclude/)");
+                next EVENT;
+            };
+        };        
+
+        if( my $dav_entry = $upstream_events{ $uid }) {
             # Well, determine if really different, also determine what changed
             # and then synchronize the two
-            print "$name exists\n";
-            add_event( $CalDAV, $davcalendar, $event );
+            if( entry_is_different( $dav_entry, $event )) {
+                verbose( "$name exists and is different, updating" );
+                update_event( $CalDAV, $dav_entry->{href}, $event );
+                #die Dumper $dav_entry;
+            } else {
+                verbose( "$name exists and is the same in CalDAV" );
+            };
+
+        } elsif( $dav_deleted{ $uid } ) {
+            verbose("Skipping locally deleted event $name");
+
         } else {
-            #warn sprintf "%s at %s", $meetup->{name}, strftime( '%Y%m%dT%H%M%SZ', gmtime( $start_epoch ));
+            verbose("Found new entry $name, adding");
             add_event( $CalDAV, $davcalendar, $event );
+            #warn sprintf "%s at %s", $meetup->{name}, strftime( '%Y%m%dT%H%M%SZ', gmtime( $start_epoch ));
         };
+        #die;
     };
     #print sprintf "%d seconds taken to sync $url", time - $fb_sync;
+    if( $sync_file and my $token = $calendar->{syncToken}) {
+        open my $fh, '>', $sync_file
+            or warn "Couldn't create timestamp file '$sync_file'";
+        binmode $fh;
+        print $fh $token;
+    };
 };
+
---
+++ lib/Meetup/ToICal.pm
@@ -0,0 +1,103 @@
+package Meetup::ToICal;
+#!perl -w
+use strict;
+use Filter::signatures;
+use feature 'signatures';
+no warnings 'experimental::signatures';
+use Exporter 'import';
+use POSIX qw(strftime);
+use Data::ICal::Entry::Event;
+use Date::ICal;
+
+our @EXPORT_OK = qw(meetup_to_icalendar get_meetup_event_uid);
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Meetup::ToICal - convert Meetup data to ICal data
+
+=head1 SYNOPSIS
+
+  use Meetup::ToICal 'meetup_to_icalendar';
+  my $ical = meetup_to_icalendar( $meetup );
+
+=head1 DESCRIPTION
+
+Import meetings from Meetup into iCal or CalDAV.
+
+=head1 FUNCTIONS
+
+=head2 C<< get_meetup_event_uid( $event ) >>
+
+  my $uid = get_meetup_event_uid( $meetup )
+
+Returns a unique identifier for a Meetup event. Currently this is the
+Meetup event id plus C<@meetup.com>.
+
+=cut
+
+sub get_meetup_event_uid( $event ) {;
+    my $uid = $event->{id} . '@meetup.com';
+}
+
+=head2 C<< meetup_to_icalendar( $event ) >>
+
+  my $data = meetup_to_icalendar( $event );
+
+Returns a data structure suitable for inserting to a CalDAV calendar
+from a Meetup event.
+
+=cut
+
+sub meetup_to_icalendar( $meetup ) {
+    my $uid = get_meetup_event_uid( $meetup );
+    if( $meetup->{time} !~ /^(\d+)\d\d\d$/ ) {
+        warn "Weirdo timestamp '$meetup->{time}' for event";
+        return;
+    };
+    my $start_epoch = $1;
+    
+    # We chuck everything into the floating "local" timezone
+    my $startTime = strftime( '%Y-%m-%dT%H:%M:%SZ', gmtime( $start_epoch + ($meetup->{utc_offset} / 1000)));
+    my $createdTime = strftime( '%Y-%m-%dT%H:%M:%SZ', gmtime( $meetup->{created} / 1000));
+
+    use Data::Dumper;
+    #warn Dumper $meetup->{venue}->{address_1};
+    #use Encode 'encode';
+    #$meetup->{venue}->{address_1} = encode('Latin-1',$meetup->{venue}->{address_1});
+    #warn "Encoded Latin-1: " . Dumper $meetup->{venue}->{address_1};
+    
+    my $res = {
+        uid      => $uid,
+        #summary  => $meetup->{name},
+        title    => $meetup->{name},
+        #description => $meetup->{description}, # Net::CalDAVTalk doesn't like the encoding sometimes and then crashes
+        created => $createdTime,
+        start  => $startTime,
+        #dtend    => $startTime, 
+        #duration => 3600, # Net::CalDAVTalk crashes if we set 'duration'?!
+        links => {
+            $meetup->{link} => {
+                href => $meetup->{link},
+                title => 'Meetup Link',
+            },
+        },
+        locations => {
+            location => {
+                name => join( "\n",
+                            grep { /\S/ }
+                                $meetup->{venue}->{name},
+                                $meetup->{venue}->{address_1},
+                                $meetup->{venue}->{city}),
+                address => { name => 'address', value => join "\n",
+                            grep { /\S/ }
+                                $meetup->{venue}->{address_1},
+                                $meetup->{venue}->{city}
+                            },
+            },
+        },
+    };
+}
+
+1;
